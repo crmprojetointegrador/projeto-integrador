3fd8c537af75ec8afec786db0532fa34eb4d68d1
Bruna Mendes <brunadsantos.mendes@gmail.com>
2026-05-26 15:16:54 -0300

    Criação da classe model

---
+++ crm_cobranca/src/main/java/com/generation/crm_cobranca/CrmCobrancaApplication.java
@@ -0,0 +1,13 @@
+package com.generation.crm_cobranca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmCobrancaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrmCobrancaApplication.class, args);
+	}
+
+}
