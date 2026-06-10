e2b3909a46a1051abad5611786ba06f2c7272e08
Bruna Mendes <brunadsantos.mendes@gmail.com>
2026-06-10 09:56:21 -0300

    Correção no relacionamento na camada de usuario

--- crm_cobranca/src/main/java/com/generation/crm_cobranca/model/Usuario.java
+++ crm_cobranca/src/main/java/com/generation/crm_cobranca/model/Usuario.java
@@ -2,13 +2,19 @@ package com.generation.crm_cobranca.model;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 
 import org.hibernate.annotations.UpdateTimestamp;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
@@ -47,7 +53,9 @@ public class Usuario {
 	@NotBlank(message = "O status é obrigatório!")
 	private String tipo; // se admin, se user
 	
-	
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "usuario", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("usuario")
+	private List<Produto> produtos;
 	
 	@UpdateTimestamp
 	private LocalDateTime data;
