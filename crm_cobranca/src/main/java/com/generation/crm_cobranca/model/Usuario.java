7d6af7ee35493c9af19797ebab102b76d3541f2c
Eliane Orlandin <liorlandin33@gmail.com>
2026-06-10 11:03:05 -0300

    feat: implementa configuração do Swagger para documentação da API e adiciona anotações no modelo Usuario

--- crm_cobranca/src/main/java/com/generation/crm_cobranca/model/Usuario.java
+++ crm_cobranca/src/main/java/com/generation/crm_cobranca/model/Usuario.java
@@ -5,6 +5,7 @@ import java.time.LocalDateTime;
 
 import org.hibernate.annotations.UpdateTimestamp;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -25,6 +26,7 @@ public class Usuario {
 	@GeneratedValue(strategy = GenerationType.IDENTITY) 
 	private Long id;
 	
+	@Schema(example = "12345678901")
 	@NotBlank(message = "O atributo CPF é obrigatório!")
 	@Size(min = 11, max = 11, message = "O atributo CPF deve ter 11 caracteres.")
 	private String cpf; // Alterado para String para aceitar zeros à esquerda e validação de tamanho
